bb30534e29318c6dcd8d1c9f41e67c42e35010a1
Behzad-Az <behy1368@gmail.com>
2023-12-19 18:50:35 -0800

    fixed minor issue on is free checkbox

--- components/shared/BuildingReportForm.tsx
+++ components/shared/BuildingReportForm.tsx
@@ -186,7 +186,7 @@ const BuildingReportForm = ({ userId, type }: BuildingReportFormProps) => {
                           <FormControl>
                             <div className="flex items-center">
                               <label htmlFor="isFree" className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Free</label>
-                              <Checkbox id="isFree" className="mr-2 h-5 w-5 border-2 border-primary-500" />
+                              <Checkbox id="isFree" className="mr-2 h-5 w-5 border-2 border-primary-500" onCheckedChange={field.onChange} checked={field.value} />
                             </div>
                           </FormControl>
                           <FormMessage />
