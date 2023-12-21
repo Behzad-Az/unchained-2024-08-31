caa48262ef5a23e987f26ea7d47ff61090f2dfa3
Behzad-Az <behy1368@gmail.com>
2023-12-20 23:26:13 -0800

    minor styling tweak

--- components/shared/TestimonialSlider.tsx
+++ components/shared/TestimonialSlider.tsx
@@ -4,7 +4,7 @@ import Carousel from "nuka-carousel"
 import { testimonials } from "@/constants"
 
 const SliderItem = ({ submitter, content }: { submitter: string, content: string}) => (
-  <div className="px-16 lg:px-[150px] xl:px-[250px] flex flex-col justify-center h-full pb-10">
+  <div className="px-10 sm:px-16 lg:px-[150px] xl:px-[250px] flex flex-col justify-center h-full pb-10">
     <p className="text-center text-lg font-semibold">" {content} "</p>
     <p className="text-center text-base mt-5">{submitter}</p>
   </div>
@@ -12,25 +12,25 @@ const SliderItem = ({ submitter, content }: { submitter: string, content: string
 
 const TestimonialSlider = () => {
   return (
-    <section className="w-full bg-primary-50 bg-dotted-pattern bg-contain py-8 px-8">
+    <section className="w-full bg-primary-50 bg-dotted-pattern bg-contain py-8 px-2 sm:px-8">
       <Carousel 
         wrapAround
         defaultControlsConfig={{
           nextButtonText: ">",
           nextButtonStyle: {
-            borderRadius: 50,
-            width: 50,
-            height: 50,
+            borderRadius: 40,
+            width: 40,
+            height: 40,
             padding: 0,
-            fontSize: 30
+            fontSize: 20
           },
           prevButtonText: "<",
           prevButtonStyle: {
-            borderRadius: 50,
-            width: 50,
-            height: 50,
+            borderRadius: 40,
+            width: 40,
+            height: 40,
             padding: 0,
-            fontSize: 30
+            fontSize: 20
           },
           pagingDotsStyle: {
             marginRight: 5,
